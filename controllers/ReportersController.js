0f0311f1b2df537877164d9dd31d298877c682c4
monty rohde <nathanarohde@gmail.com>
2015-02-27 14:29:56 -0800

    Assigns reporters to a division.

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,7 +1,9 @@
 newspaperRoster.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, DivisionsFactory, UtilitiesFactory){
-    $scope.division = UtilitiesFactory.findById(DivisionsFactory.divisions, $stateParams.divisionId)
+    $scope.division = UtilitiesFactory.findById(DivisionsFactory.divisions, $stateParams.divisionId);
     $scope.addReporter = function() {
       $scope.division.reporters.push({name: $scope.reporterName, yearsOfService: $scope.reporterYearsOfService, birthDay: $scope.reporterBirthDay});
       $scope.reporterName = null;
+      $scope.reporterYearsOfService = null;
+      $scope.reporterBirthDay = null;
     }
 });
